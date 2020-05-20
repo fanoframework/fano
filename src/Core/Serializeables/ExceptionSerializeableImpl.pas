ee1622c4527564abf1bcbec4fc3a722e5f4f909b
Zamrony P. Juhara <zamronypj@yahoo.com>
2020-05-20 10:08:55 +0800

    add adapter class to allow Exception to be use as ISerializeable

---
+++ src/Core/Serializeables/ExceptionSerializeableImpl.pas
@@ -0,0 +1,48 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit ExceptionSerializeableImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    SysUtils,
+    InjectableObjectImpl,
+    SerializeableIntf;
+
+type
+    (*!------------------------------------------------
+     * adapter class that implements ISerializeable which wraps
+     * Exception
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *--------------------------------------------------*)
+    TExceptionSerializeble = class(TInjectableObject, ISerializeable)
+    private
+        fExcept : Exception;
+    public
+        constructor create(const aExcept : Exception);
+        function serialize() : string;
+    end;
+
+implementation
+
+    constructor TExceptionSerializeble.create(const aExcept : Exception);
+    begin
+        fExcept := aExcept;
+    end;
+
+    function TExceptionSerializeble.serialize() : string;
+    begin
+        result := fExcept.ToString;
+    end;
+end.
