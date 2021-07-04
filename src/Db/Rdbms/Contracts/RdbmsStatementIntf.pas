330d5b4d410676936b72651ead71d8aebfeca533
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-07-04 23:29:23 +0700

    improve setting parameter on db prepared statement

--- src/Db/Rdbms/Contracts/RdbmsStatementIntf.pas
+++ src/Db/Rdbms/Contracts/RdbmsStatementIntf.pas
@@ -43,11 +43,53 @@ type
         function paramStr(const strName : string; const strValue : string) : IRdbmsStatement;
 
         (*!------------------------------------------------
+         * set parameter UTF8 string value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramUtf8(const strName : string; const strValue : utf8string) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter unicode string value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramUnicode(const strName : string; const strValue : unicodestring) : IRdbmsStatement;
+
+        (*!------------------------------------------------
          * set parameter integer value
          *-------------------------------------------------
          * @return current statement
          *-------------------------------------------------*)
         function paramInt(const strName : string; const strValue : integer) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter small integer value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramSmallInt(const strName : string; const strValue : smallint) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter large integer (64-bit)value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramLargeInt(const strName : string; const strValue : largeint) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter word value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramWord(const strName : string; const strValue : word) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter boolean value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramBool(const strName : string; const strValue : boolean) : IRdbmsStatement;
 
         (*!------------------------------------------------
          * set parameter float value
@@ -57,11 +99,39 @@ type
         function paramFloat(const strName : string; const strValue : double) : IRdbmsStatement;
 
         (*!------------------------------------------------
+         * set parameter currency value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramCurrency(const strName : string; const strValue : currency) : IRdbmsStatement;
+
+        (*!------------------------------------------------
          * set parameter datetime value
          *-------------------------------------------------
          * @return current statement
          *-------------------------------------------------*)
         function paramDateTime(const strName : string; const strValue : TDateTime) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter date value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramDate(const strName : string; const strValue : TDateTime) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter time value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramTime(const strName : string; const strValue : TDateTime) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter time value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramBytes(const strName : string; const strValue : TBytes) : IRdbmsStatement;
     end;
 
 implementation
--- src/Db/Rdbms/Implementations/Base/RdbmsImpl.pas
+++ src/Db/Rdbms/Implementations/Base/RdbmsImpl.pas
@@ -206,11 +206,53 @@ type
         function paramStr(const strName : string; const strValue : string) : IRdbmsStatement;
 
         (*!------------------------------------------------
+         * set parameter UTF8 string value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramUtf8(const strName : string; const strValue : utf8string) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter unicode string value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramUnicode(const strName : string; const strValue : unicodestring) : IRdbmsStatement;
+
+        (*!------------------------------------------------
          * set parameter integer value
          *-------------------------------------------------
          * @return current statement
          *-------------------------------------------------*)
         function paramInt(const strName : string; const strValue : integer) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter small integer value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramSmallInt(const strName : string; const strValue : smallint) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter large integer (64-bit)value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramLargeInt(const strName : string; const strValue : largeint) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter word value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramWord(const strName : string; const strValue : word) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter boolean value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramBool(const strName : string; const strValue : boolean) : IRdbmsStatement;
 
         (*!------------------------------------------------
          * set parameter float value
@@ -220,11 +262,39 @@ type
         function paramFloat(const strName : string; const strValue : double) : IRdbmsStatement;
 
         (*!------------------------------------------------
+         * set parameter currency value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramCurrency(const strName : string; const strValue : currency) : IRdbmsStatement;
+
+        (*!------------------------------------------------
          * set parameter datetime value
          *-------------------------------------------------
          * @return current statement
          *-------------------------------------------------*)
         function paramDateTime(const strName : string; const strValue : TDateTime) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter date value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramDate(const strName : string; const strValue : TDateTime) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter time value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramTime(const strName : string; const strValue : TDateTime) : IRdbmsStatement;
+
+        (*!------------------------------------------------
+         * set parameter time value
+         *-------------------------------------------------
+         * @return current statement
+         *-------------------------------------------------*)
+        function paramBytes(const strName : string; const strValue : TBytes) : IRdbmsStatement;
     end;
 
 implementation
@@ -555,6 +625,30 @@ resourcestring
     end;
 
     (*!------------------------------------------------
+     * set parameter UTF8 string value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramUtf8(const strName : string; const strValue : utf8string) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asUtf8String := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * set parameter Unicode string value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramUnicode(const strName : string; const strValue : unicodestring) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asUtf8String := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
      * set parameter integer value
      *-------------------------------------------------
      * @return current statement
@@ -563,6 +657,54 @@ resourcestring
     begin
         raiseExceptionIfInvalidConnection();
         fQuery.params.paramByName(strName).asInteger := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * set parameter small integer value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramSmallInt(const strName : string; const strValue : smallint) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asSmallInt := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * set parameter word value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramWord(const strName : string; const strValue : word) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asWord := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * set parameter large integer (64-bit) value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramLargeInt(const strName : string; const strValue : largeint) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asLargeInt := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * set parameter boolean value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramBool(const strName : string; const strValue : boolean) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asBoolean := strValue;
         result := self;
     end;
 
@@ -579,6 +721,18 @@ resourcestring
     end;
 
     (*!------------------------------------------------
+     * set parameter currency value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramCurrency(const strName : string; const strValue : currency) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asCurrency := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
      * set parameter datetime value
      *-------------------------------------------------
      * @return current statement
@@ -587,6 +741,42 @@ resourcestring
     begin
         raiseExceptionIfInvalidConnection();
         fQuery.params.paramByName(strName).asDateTime := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * set parameter date value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramDate(const strName : string; const strValue : TDateTime) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asDate := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * set parameter time value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramTime(const strName : string; const strValue : TDateTime) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asTime := strValue;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * set parameter bytes value
+     *-------------------------------------------------
+     * @return current statement
+     *-------------------------------------------------*)
+    function TRdbms.paramBytes(const strName : string; const strValue : TBytes) : IRdbmsStatement;
+    begin
+        raiseExceptionIfInvalidConnection();
+        fQuery.params.paramByName(strName).asBytes := strValue;
         result := self;
     end;
 end.
